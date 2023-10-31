58a14e5ab142d6e46824d7248c759b1be283c6af
abelshebela <abelshebela@gmail.com>
2023-10-31 22:43:47 +0300

    modified the db.php with changing the host, username, password and database with the MYSQL server credintials and added the .github/workflows directory with the code to import the MYsql server database to Azure flexible mysql server

--- includes/db.php
+++ includes/db.php
@@ -1,13 +1,16 @@
 <?php
-$host = "localhost"; // Typically 'localhost'
-$username = "root";
-$password = "";
-$database = "finance_tracker";
+<?php
+$server = "DESKTOP-55K5Q8Q"; // Your local MySQL server hostname
+$username = "abbk"; // Your local MySQL server username
+$password = "abbk@2023"; // Your local MySQL server password
+$database = "finance_tracker"; // The name of your database
+
 
 // Create a database connection
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($server, $username, $password, $database);
 
 // Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+?>

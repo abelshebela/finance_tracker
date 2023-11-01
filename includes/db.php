e517e76fe2fdf346ff87162e46e732328e4ca07e
abelshebela <abelshebela@gmail.com>
2023-11-01 20:00:41 +0300

    modified the db.php

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
-$server = "DESKTOP-55K5Q8Q"; // Your local MySQL server hostname
-$username = "abbk"; // Your local MySQL server username
-$password = "abbk@2023"; // Your local MySQL server password
-$database = "finance_tracker"; // The name of your database
+$server = "project-phase2.mysql.database.azure.com"; // Your local MySQL server hostname
+$username = "projectphase2"; // Your local MySQL server username
+$password = "Nedamcoacademy2"; // Your local MySQL server password
+$database = "finance-tracker"; // The name of your database
 
 
 // Create a database connection
@@ -11,6 +11,5 @@ $conn = new mysqli($server, $username, $password, $database);
 // Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
-
+}
 ?>
